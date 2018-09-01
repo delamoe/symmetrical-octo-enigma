9ccefd1f5f468cfb1144b5f035c936cbc0bb67f7
delamoe <delamoe@users.noreply.github.com>
2018-09-01 23:14:21 +0300

    started AddThreeDigits

---
+++ Exercises/AddThreeDigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AddThreeDigits{
+    public static void main(String[] args) {
+        // initialize scanner
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            // initialize three random ints
+            
+        }
+    }    
+}
