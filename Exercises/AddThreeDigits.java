29ddb54190858febb2613d3abf7a0f0ee8a81cd4
delamoe <delamoe@users.noreply.github.com>
2018-09-01 23:34:18 +0300

    finished AddThreeDigits

--- Exercises/AddThreeDigits.java
+++ Exercises/AddThreeDigits.java
@@ -5,9 +5,24 @@ public class AddThreeDigits{
         // initialize scanner
         Scanner input = new Scanner(System.in);
 
+       int[] array = new int[3];
+
         for (int i = 0; i < 3; i++) {
             // initialize three random ints
-            
+            array[i] = (int) (Math.random() * 10);
+        }
+
+        System.out.printf("%nWhat is %d + %d + %d?%n",array[0], array[1], array[2]);
+
+        int answer = input.nextInt();
+
+        String result = (answer == array[0] + array[1] + array[2]) ? "correct" : "incorrect";
+
+        System.out.printf("%nYou have given the %s answer. ",result);
+        if (result == "correct"){
+            System.out.printf("Good Job!%n");
+        } else {
+            System.out.printf("The correct answer is %d%n",array[0] + array[1] + array[2]);
         }
     }    
 }
