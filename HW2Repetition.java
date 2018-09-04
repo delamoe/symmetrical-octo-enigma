786ba15c946f4a0a0fecd54acdc8cc150d36ea46
delamoe <delamoe@users.noreply.github.com>
2018-09-04 11:44:46 +0300

    finishing up decorations

--- HW2Repetition.java
+++ HW2Repetition.java
@@ -156,26 +156,28 @@ public class HW2Repetition {
                 //
                 grandTotal = (subTotal - (subTotal * discount) + shippingCost);
 
-                System.out.printf("%n%nYour shopping cart contains %d item%s.%n", cartTotalItems, plural);
+                String cartOrShipment = !finishedShopping ? "shopping cart" : "shipment";
+
+                System.out.printf("%n%n  Your %s contains %d item%s.%n", cartOrShipment, cartTotalItems, plural);
                 if (subTotal > 0) {
                     System.out.printf(
-                            "Qty\tDescription\t\tPrice\t    Total%n-------------------------------------------------");
+                            "| Qty\tDescription\t\tPrice\t    Total |%n|-------------------------------------------------|");
                     for (int i = 0; i <= (cartIndex - 1); i++) {
                         //
-                        System.out.printf("%n%s\t%s\t$%4.2f\t$%8.2f", cart[1][i], productID[1][cart[0][i]],
+                        System.out.printf("%n| %s\t%s\t$%4.2f\t$%8.2f |", cart[1][i], productID[1][cart[0][i]],
                                 productPrice[cart[0][i]], (cart[1][i] * productPrice[cart[0][i]]));
                     }
-                    System.out.printf("%n-------------------------------------------------");
+                    System.out.printf("%n|-------------------------------------------------|");
                 }
 
                 // cartTotalItems will be recalculated in next iteration
                 cartTotalItems = 0;
                 if (finishedShopping && subTotal > 0) {
                     System.out.printf(
-                            "%nPrice of products\t\t\t$%8.2f%nDiscount %2.0f%%\t\t\t\t$%8.2f%nNet purchase amount\t\t\t$%8.2f%nShipping cost\t\t\t\t$%8.2f%nTotal\t\t\t\t\t$%8.2f%n",
+                            "%n| Price of products\t\t\t$%8.2f |%n| Discount %2.0f%%\t\t\t\t$%8.2f |%n| Net purchase amount\t\t\t$%8.2f |%n| Shipping cost\t\t\t\t$%8.2f |%n| Total\t\t\t\t\t$%8.2f |",
                             subTotal, discount * 100, subTotal * discount, subTotal - subTotal * discount, shippingCost,
                             grandTotal);
-                    System.out.printf("-------------------------------------------------%n");
+                    System.out.printf("%n|-------------------------------------------------|%n");
                 }
             }
         }
