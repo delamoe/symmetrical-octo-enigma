09b8043633a25c9965caccf14f44069cec162ab9
delamoe <delamoe@users.noreply.github.com>
2018-10-25 20:35:30 +0300

    Started EX4

---
+++ EX4_RandomAccessFiles.java
@@ -0,0 +1,25 @@
+/*
+ * EX4 - Random Access Files Instructions
+ * 
+ * You have an object of the RandomAccessFile class named empRaf. It refers to a physical file that contains a company's employee records. Each employee record has:
+ * 
+ *    an int field named empID which points to its location in the file
+ * 
+ *    a 20 character last name
+ * 
+ *    a 15 character first_name
+ * 
+ *    a hire date stored as an int
+ * 
+ *    a 3 character job code
+ * 
+ * The empID values begin at 1, but the first record in the file begins at byte 0.
+ * 
+ * 1. Calculate the length of this record.
+ * 
+ * 2. Write a statement that positions the file pointer at the beginning of the record that has empID 20.
+ * 
+ * Be sure to read the document in the content module and study the sample progrms before trying to answer this.
+ * 
+ * Due Date Dec 2, 2018 11:59 PM
+ */
