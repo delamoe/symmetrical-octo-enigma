6fd43bdbd619ae16eaabba74e82d1878ec11f87f
delamoe <delamoe@users.noreply.github.com>
2018-11-22 08:22:37 +0300

    restarting HW10

--- HW10_/src/Book.java
+++ HW10_/src/Book.java
@@ -1,67 +1,68 @@
-/*
-* Class      : Book.java
-*
-* Purpose    : HW10 - File Operations
-*              10979 CIS131 Prog & Problem Solv II
-*
-* Developer  : E de la Montaña
-*
-* Date       : 10/25/2018
-* 
-*/
 
+/*
+     * Class	:	Book.java
+     *
+     * Purpose 	:	HW8 Objects Part 3 Inheritance and Polymorphisms
+     * 			10979 CIS131 Prog & Problem Solv II 
+     *
+     * Developer:	E de la Montaña
+     *
+     * Date	:	10/25/2018
+     */
 
 class Book extends Publication {
 
-  private String author;
-  private String isbn;
-  private int libraryOfCongressNum;
-  private int pages;
+        private String author;
+        private String isbn;
+        private int libraryOfCongressNum;
+        private int pages;
 
-  public Book(String title, String publisher, String publicationDate, String subject, String author, String isbn,
-      int libraryOfCongressNum, int pages) {
-    super(title, publisher, publicationDate, subject);
-    setAuthor(author);
-    setIsbn(isbn);
-    setLibraryOfCongressNum(libraryOfCongressNum);
-    setPages(pages);
-  }
+        public Book(String title, String publisher, String publicationDate, String subject, String author, String isbn,
+                        int libraryOfCongressNum, int pages) {
+                super(title, publisher, publicationDate, subject);
+                setAuthor(author);
+                setIsbn(isbn);
+                setLibraryOfCongressNum(libraryOfCongressNum);
+                setPages(pages);
+        }
 
-  public String getAuthor() {
-    return this.author;
-  }
+        public String getAuthor() {
+                return this.author;
+        }
 
-  public void setAuthor(String author) {
-    this.author = author;
-  }
+        public void setAuthor(String author) {
+                this.author = author;
+        }
 
-  public String getIsbn() {
-    return this.isbn;
-  }
+        public String getIsbn() {
+                return this.isbn;
+        }
 
-  public void setIsbn(String isbn) {
-    this.isbn = isbn;
-  }
+        public void setIsbn(String isbn) {
+                this.isbn = isbn;
+        }
 
-  public int getLibraryOfCongressNum() {
-    return this.libraryOfCongressNum;
-  }
+        public int getLibraryOfCongressNum() {
+                return this.libraryOfCongressNum;
+        }
 
-  public void setLibraryOfCongressNum(int libraryOfCongressNum) {
-    this.libraryOfCongressNum = libraryOfCongressNum;
-  }
+        public void setLibraryOfCongressNum(int libraryOfCongressNum) {
+                this.libraryOfCongressNum = libraryOfCongressNum;
+        }
 
-  public int getPages() {
-    return this.pages;
-  }
+        public int getPages() {
+                return this.pages;
+        }
 
-  public void setPages(int pages) {
-    this.pages = pages;
-  }
+        public void setPages(int pages) {
+                this.pages = pages;
+        }
 
-  public String toString() {
-    return "Book :" + super.toString() + "\nAuthor. . . . . . . . . . . . " + author
-        + "\nISBN . . . . . . . . . . . . ." + isbn + "\nLibrary of Congress Number. . " + libraryOfCongressNum
-        + "\nPages. . . . . . . . . . . . ." + pages;
-  }
+        public String toString() {
+                return "Book :" + super.toString() +
+                "\nAuthor. . . . . . . . . . . . " + author +
+                "\nISBN . . . . . . . . . . . . ." + isbn +
+                "\nLibrary of Congress Number. . " + libraryOfCongressNum +
+                "\nPages. . . . . . . . . . . . ." + pages;
+        }
 }
--- HW10_/src/Driver.java
+++ HW10_/src/Driver.java
@@ -1,55 +1,41 @@
-
 /*
-* Class      : Driver.java
-*
-* Purpose    : HW10 - File Operations
-*              10979 CIS131 Prog & Problem Solv II
-*
-* Developer  : E de la Montaña
-*
-* Date       : 10/25/2018
-* 
+     * Class	  :	Driver.java
+     *
+     * Purpose 	:	HW8 Objects Part 3 Inheritance and Polymorphism
+     *            10979 CIS131 Prog & Problem Solv II 
+     *
+     * Developer:	E de la Montaña
+     *
+     * Date		  :	10/28/2018
 */
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Driver {
   public static void main(String[] args) {
-
-    final String BOOK_FILE = "BookFile.dat";
-
-    RandomAccessFile bookFile = null;
-
+    
     List<Publication> publications = new ArrayList<>();
+    
+    Publication[] publicationEntries = new Publication[] { 
+      new Book("The Dark Tower", "Donald M. Grant", "1982", "Science Fiction - Fantasy", "Stephen King", "9780743254564", 2006271051, 845), 
+      new Book("Foundation and Earth", "Doubleday", "1986", "Science Fiction", "Issac Asimov", "0246130474", 87162264, 462), 
+      new Book("Macroscope", "Avon Books", "1969", "Science Fiction", "Piers Anthony", "0839828993", 85021860, 480),
+      new Book("Imajica", "HarperCollins", "1991", "Fantasy", "Clive Barker", "0060937262", 2001059130, 880), 
+      new Book("The Dosadi Experiment", "G. P. Putnam's Sons", "1977", "Science Fiction", "Frank Herbert", "039912022X", 77003653, 336)
+    };
 
-    try {
-      bookFile = new RandomAccessFile(BOOK_FILE, "rw");
-    }
-
-    catch (IOException e) {
-      System.out.println("\n IOExeption ocurred while attempting to allocate the BookFile.");
-    }
-
-    Publication[] publicationEntries = new Publication[] {
-        new Book("The Dark Tower", "Donald M. Grant", "1982", "Fantasy", "Stephen King", "9780743254564", 2006271051, 845),
-        new Book("Foundation and Earth", "Doubleday", "1986", "Science Fiction", "Issac Asimov", "0246130474", 87162264, 462),
-        new Book("Macroscope", "Avon Books", "1969", "Science Fiction", "Piers Anthony", "0839828993", 85021860, 480),
-        new Book("Imajica", "HarperCollins", "1991", "Fantasy", "Clive Barker", "0060937262", 2001059130, 880),
-        new Book("The Dosadi Experiment", "G. P. Putnam's Sons", "1977", "Science Fiction", "Frank Herbert", "039912022X", 77003653, 336)
-          };
-
+    /* for (Publication publication : publicationEntries) {
+      publications.add(publication);
+      System.out.println(publication.toString() + "\n");
+    } */
 
     for (Publication publication : publicationEntries) {
       publications.add(publication);
     }
-
-    for (Publication publication : publications) {
-      // System.out.println(publication.toString() + "\n");
-      bookFile.add(publication);
-    }
     
+    for (Publication publication : publications) {
+      System.out.println(publication.toString() + "\n");
+    }
   }
 }
